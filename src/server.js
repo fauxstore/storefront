242e0fe4223a91d2ee1943b88673b38e96e720c1
Pete Hodgson <git@thepete.net>
2020-09-13 19:23:32 -0700

    HTML!

--- src/server.js
+++ src/server.js
@@ -3,15 +3,17 @@ require('express-async-errors');
 
 module.exports = function createServer({port,dealsGateway}){
   const app = express()
+  app.set('view engine', 'hbs');
+  app.set('views', __dirname + '/views');
+
 
   app.get('/', async (req, res) => {
     const response = await dealsGateway.getDeals();
     const deals = response.data;
-    console.debug({deals});
-    const renderedDeals = deals.map( (deal)=>{
-      return `${deal.description}: (${deal.promoCode})\n`;
+
+    res.render('home',{
+      deals
     });
-    res.send('Welcome to the faux store!\n\n'+renderedDeals);
   });
 
   app.listen(port, () => {
